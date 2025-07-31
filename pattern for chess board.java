432fcfea0475e105009e9946ce07b649f0a0dafc
Revathigovinth <162727157+Revathigovinth@users.noreply.github.com>
2025-07-31 21:56:21 +0530

    Create pattern for chess board.java

---
+++ pattern for chess board.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n,row,col;
+        n=sc.nextInt();
+        for(row=1;row<=n;row++){
+            for(col=1;col<=n;col++){
+                if((row+col)%2==0){
+                    System.out.print("1");
+                }
+                else{
+                    System.out.print("0");
+                }
+            }
+             System.out.println();
+        }
+    }
+}
