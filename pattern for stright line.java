116244784bd03f822bb24ca2cc6c69942c99850f
Revathigovinth <162727157+Revathigovinth@users.noreply.github.com>
2025-07-31 21:55:01 +0530

    Create  pattern for stright line.java

---
+++ pattern for stright line.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n,row,col;
+        n=sc.nextInt();
+        for(row=1;row<=n;row++){
+            for(col=1;col<=n;col++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
